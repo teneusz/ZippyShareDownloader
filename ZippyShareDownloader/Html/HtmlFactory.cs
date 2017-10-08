119b2e39c08a555bb49c1ca0d35be61ded073e16
Teneusz <teneusz@wp.pl>
2017-10-08 17:48:47 +0200

    Pasting URLs without http* prefix cause app crash #1

--- ZippyShareDownloader/Html/HtmlFactory.cs
+++ ZippyShareDownloader/Html/HtmlFactory.cs
@@ -17,7 +17,7 @@ namespace ZippyShareDownloader.Html
     {
         private static readonly Dictionary<string, IHtmlLinkCutter> FactoryHtml =
             new Dictionary<string, IHtmlLinkCutter>();
-        private static readonly ILog log = LogManager.GetLogger(typeof(HtmlFactory));
+        private static readonly ILog Log = LogManager.GetLogger(typeof(HtmlFactory));
 
         static HtmlFactory()
         {
@@ -28,29 +28,29 @@ namespace ZippyShareDownloader.Html
 
         public static (string service, string parsedLink, string fileName) ParseLink(string link)
         {
-            log.Debug("start -- ParseLink(string)");
-            log.Debug("link = "+link);
+            Log.Debug("start -- ParseLink(string)");
+            Log.Debug("link = "+link);
             var service = GetService(link);
             var htmlCutter = FactoryHtml[service];
             var parsedLink = htmlCutter != null ? htmlCutter.GetDirectLinkFromLink(link) : link;
             var fileName = System.Net.WebUtility.UrlDecode(GetFileName(parsedLink));
-            log.Debug("end -- ParseLink(String)");
+            Log.Debug("end -- ParseLink(String)");
             return (service, parsedLink, fileName);
         }
 
         private static string GetFileName(string link)
         {
-            log.Debug("start -- GetFileName(String)");
-            log.Debug("link = " + link);
+            Log.Debug("start -- GetFileName(String)");
+            Log.Debug("link = " + link);
             var lastSlashIndex = link.LastIndexOf('/') + 1;
-            log.Debug("end -- GetFileName(string)");
+            Log.Debug("end -- GetFileName(string)");
             return link.Substring(lastSlashIndex, link.Length - lastSlashIndex);
         }
 
         private static string GetService(string link)
         {
-            log.Debug("start -- GetService(String)");
-            log.Debug("link = "+link);
+            Log.Debug("start -- GetService(String)");
+            Log.Debug("link = "+link);
             var result = "";
             if (link.ToLower().Contains(Http))
             {
@@ -67,7 +67,7 @@ namespace ZippyShareDownloader.Html
                 if (s.Length > result.Length)
                     result = s;
             }
-            log.Debug("end -- GetFileName(String)");
+            Log.Debug("end -- GetFileName(String)");
             return result;
         }
 
--- ZippyShareDownloader/View/AddLinks.xaml.cs
+++ ZippyShareDownloader/View/AddLinks.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using log4net;
 using ZippyShareDownloader.Entity;
+using ZippyShareDownloader.Html;
 
 namespace ZippyShareDownloader.View
 {
@@ -22,7 +23,8 @@ namespace ZippyShareDownloader.View
     public partial class AddLinks : Window
     {
         private readonly MainViewModel _viewModel = MainViewModel.InstatnceMainViewModel;
-        private static readonly ILog log = LogManager.GetLogger(typeof(AddLinks));
+        private static readonly ILog Log = LogManager.GetLogger(typeof(AddLinks));
+
         public AddLinks()
         {
             InitializeComponent();
@@ -30,17 +32,22 @@ namespace ZippyShareDownloader.View
 
         private void SaveOnClick(object sender, RoutedEventArgs e)
         {
-            log.Debug("start -- saveOnClick");
+            Log.Debug("start -- saveOnClick");
             var textRange = new TextRange(TextBox.Document.ContentStart, TextBox.Document.ContentEnd);
             var tab = textRange.Text.Split('\n').Select(link => link.Trim()).ToList();
 
             foreach (var s in tab)
             {
+                var link = s;
+                if (!link.StartsWith(HtmlFactory.Http) && !link.StartsWith(HtmlFactory.Https))
+                {
+                    link = HtmlFactory.Http + s;
+                }
                 if (s.Length > 0)
-                    _viewModel.Downloads.Add(new DownloadEntity {ServiceLink = s});
+                    _viewModel.Downloads.Add(new DownloadEntity {ServiceLink = link});
             }
             DialogResult = true;
-            log.Debug("end -- saveOnClick");
+            Log.Debug("end -- saveOnClick");
         }
 
         private void CancelOnClick(object sender, RoutedEventArgs e)
