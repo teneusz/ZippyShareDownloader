3d0daaf7e213c718ee701267bc3cd075ecfcefbc
Teneusz <teneusz@wp.pl>
2017-11-09 19:05:33 +0100

    Sharing links, copying column content #6

--- ZippyShareDownloader/View/MainWindow.xaml.cs
+++ ZippyShareDownloader/View/MainWindow.xaml.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -37,10 +38,6 @@ namespace ZippyShareDownloader
             this.DataContext = _viewModel;
         }
 
-        private void DataGrid_OnMouseDoubleClick(object sender, MouseButtonEventArgs e)
-        {
-           
-        }
     }
 
     public class ObservableCollectionEx<T> : ObservableCollection<T> where T : INotifyPropertyChanged
